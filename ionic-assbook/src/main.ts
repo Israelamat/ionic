2308a6cb7f55060ad4a02e2318f3982eb209c5d9
Israelamat <israelamatgil@gmail.com>
2024-01-27 16:11:12 +0100

    Fin

--- ionic-assbook/src/main.ts
+++ ionic-assbook/src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { PreloadAllModules, RouteReuseStrategy, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
+import { PreloadAllModules, RouteReuseStrategy, provideRouter, withComponentInputBinding, withPreloading, withRouterConfig } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 
@@ -18,7 +18,7 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules), withComponentInputBinding()),
+    provideRouter(routes, withPreloading(PreloadAllModules), withComponentInputBinding(), withRouterConfig({paramsInheritanceStrategy: 'always'})),
     provideHttpClient(withInterceptors([baseUrlInterceptor, authTokenInterceptor])),
   ],
   
